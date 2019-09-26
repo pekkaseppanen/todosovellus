476632140c672d0c8e2cd89a4ffc4819809f94d8
Seppänen K-Pekka A <kaseppan@vdi-cubic-023.ad.helsinki.fi>
2019-09-26 16:21:18 +0300

    lomakkeen käsittelyn jälkeen

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.tasks.models import Task
+from application.tasks.forms import TaskForm
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
@@ -8,7 +9,7 @@ def tasks_index():
 
 @app.route("/tasks/new/")
 def tasks_form():
-    return render_template("tasks/new.html")
+    return render_template("tasks/new.html", form = TaskForm())
 
 @app.route("/tasks/<task_id>/", methods=["POST"])
 def tasks_set_done(task_id):
@@ -18,10 +19,16 @@ def tasks_set_done(task_id):
     db.session().commit()
 
     return redirect(url_for("tasks_index"))
-    
+
 @app.route("/tasks/", methods=["POST"])
 def tasks_create():
-    t = Task(request.form.get("name"))
+    form = TaskForm(request.form)
+
+    if not form.validate():
+        return render_template("tasks/new.html", form = form)
+
+    t = Task(form.name.data)
+    t.done = form.done.data
 
     db.session().add(t)
     db.session().commit()
